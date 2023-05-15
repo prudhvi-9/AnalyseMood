18869ec91b0478923b311d59983e0880cf928589
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-05-15 11:11:06 +0530

    Added:To handle exception if user provides invalid mood.

---
+++ src/main/java/main/NullException.java
@@ -0,0 +1,11 @@
+package main;
+
+public class NullException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NullException(String message) {
+		super(message);
+	}
+
+}
